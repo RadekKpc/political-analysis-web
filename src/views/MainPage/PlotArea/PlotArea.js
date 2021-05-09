1d12964fddf38caf2fadbb0dd11b4d9b0396df98
Radoslaw Kopec <radek4ec@gmail.com>
2021-05-09 03:13:25 +0200

    Map dates labels properly

--- src/views/MainPage/PlotArea/PlotArea.js
+++ src/views/MainPage/PlotArea/PlotArea.js
@@ -46,17 +46,16 @@ function PlotArea(props) {
     let start = props.dateRange[0] ? new Date(props.dateRange[0].getTime()) : new Date();
     let end = props.dateRange[1] ? new Date(props.dateRange[1].getTime()) : new Date();
     while(end.getDate() !== start.getDate() || end.getMonth() !== start.getMonth() || end.getFullYear() !== start.getFullYear() ){
-      days.push(start.getUTCDate() + '.0' + start.getMonth());
+      days.push(end.getFullYear() + '-' + ('0' + (start.getMonth() + 1)).slice(-2) + "-" + ( "0" + start.getUTCDate()).slice(-2));
       start.setDate(start.getDate() + 1);
     }
-
-    console.log("Data", data);
+    console.log(days);
     return  {
       labels: [...days],
       datasets: data ? data.map((set) => {
         return {
           label: set[0] ? set[0].category : '',
-          data: set[0] ? set.map((e) => e.result) : [],
+          data: set[0] ? set.map((e) => { return {x: e.date, y: e.result}}) : [],
           fill: false,
           backgroundColor: getBackgroundColor(),
           borderColor: getBorderColor()
