55138bc1fc6deed4c049997f9d9d09d7c03adf91
alexmaar <oo.marzec@gmail.com>
2021-05-29 21:11:48 +0200

    Format code.

--- src/services/StaticStatisticsService.js
+++ src/services/StaticStatisticsService.js
@@ -1,107 +1,92 @@
-const getTotalCountForSpecificCategories = (
-  categories,
-  dateRange,
-  setData,
-  type
-) => {
-  let attr;
 
-  switch (type) {
-    case "TotalTweetsCount":
-      attr = "tweets";
-      break;
-    case "TotalLikesCount":
-      attr = "likes";
-      break;
-    case "TotalRetweetsCount":
-      attr = "retweets";
-      break;
-    default:
-      attr = "";
-      break;
-  }
+const getTotalCountForSpecificCategories = (categories, dateRange, setData, type) => {
 
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      categories: categories,
-      dateRange: dateRange,
-      attribute: attr,
-    }),
-  };
+    let attr;
 
-  fetch("/total-count", requestOptions)
-    .then((response) => response.json())
-    .then((data) => setData(data));
-};
+    switch (type) {
+        case "TotalTweetsCount":
+            attr = "tweets"
+            break;
+        case "TotalLikesCount":
+            attr = "likes";
+            break;
+        case "TotalRetweetsCount":
+            attr = "retweets";
+            break;
+        default:
+            attr = "";
+            break;
+    }
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            categories: categories,
+            dateRange: dateRange,
+            attribute: attr
+        })
+    };
+
+    fetch('/total-count', requestOptions)
+        .then(response => response.json())
+        .then(data => setData(data));
+}
 
 const getTweetsForDay = (categories, dateRange, setData, type) => {
-  let attr;
+    let attr;
 
-  switch (type) {
-    case "TotalTweetsCount":
-      attr = "tweets";
-      break;
-    case "TotalLikesCount":
-      attr = "likes";
-      break;
-    case "TotalRetweetsCount":
-      attr = "retweets";
-      break;
-    default:
-      attr = "";
-      break;
-  }
+    switch (type) {
+        case "TotalTweetsCount":
+            attr = "tweets"
+            break;
+        case "TotalLikesCount":
+            attr = "likes";
+            break;
+        case "TotalRetweetsCount":
+            attr = "retweets";
+            break;
+        default:
+            attr = "";
+            break;
+    }
 
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      categories: categories,
-      dateRange: dateRange,
-      attribute: attr,
-    }),
-  };
-  console.log("request");
-  fetch("/time-count", requestOptions)
-    .then((response) => response.json())
-    .then((data) => setData(data));
-};
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            categories: categories,
+            dateRange: dateRange,
+            attribute: attr
+        })
+    };
+    console.log('request');
+    fetch('/time-count', requestOptions)
+        .then(response => response.json())
+        .then(data => setData(data));
+}
 
 const getWordCloud = (categories, dateRange, setTweetText, type) => {
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      categories: categories,
-      dateRange: dateRange,
-    }),
-  };
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            categories: categories,
+            dateRange: dateRange,
+        })
+    };
 
-  fetch("/wordcloud", requestOptions)
-    .then((response) => response.json())
-    .then((data) =>
-      setTweetText(
-        data
-          .map((word) => word.split(" "))
-          .flat(1)
-          .filter(
-            (word) =>
-              !word.match(/#[a-zżźćńółęąś]+/gi) &&
-              !word.match(/@[a-zżźćńółęąś,_]+/gi)
-          )
-          .map((word) => word.replace(/"|\?|„/g, ""))
-          .join(" ")
-      )
-    );
-};
+    fetch('/wordcloud', requestOptions)
+        .then(response => response.json())
+        .then(data => setTweetText(data.join(" ")));
+
+}
 
 const getWordCloudHashtags = (categories, dateRange, setTweetText, type) => {
   const requestOptions = {
@@ -127,9 +112,5 @@ const getWordCloudHashtags = (categories, dateRange, setTweetText, type) => {
       )
     );
 };
-export {
-  getTotalCountForSpecificCategories,
-  getTweetsForDay,
-  getWordCloud,
-  getWordCloudHashtags,
-};
+
+export { getTotalCountForSpecificCategories, getTweetsForDay , getWordCloud, getWordCloudHashtags};
